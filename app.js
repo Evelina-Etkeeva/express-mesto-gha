35f8ffa5a4b6e4f9d9f5031dd6ab197e8e85aad6
Radioteddy <f.o.akhmetov@gmail.com>
2022-08-14 16:51:35 +0200

    logger is added

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const rateLimit = require('express-rate-limit');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-const helmet = require('helmet');
-const auth = require('./middlewares/auth');
-const users = require('./routes/users');
-const cards = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
-const NotFoundError = require('./errors/NotFoundErr');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const rateLimit = require("express-rate-limit");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const { celebrate, Joi, errors } = require("celebrate");
+const helmet = require("helmet");
+const auth = require("./middlewares/auth");
+const users = require("./routes/users");
+const cards = require("./routes/cards");
+const { login, createUser } = require("./controllers/users");
+const NotFoundError = require("./errors/NotFoundErr");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
-
-const app = express();
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -24,6 +23,9 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
+const app = express();
+
+// защищаемся от атак
 app.use(limiter);
 app.use(helmet());
 // для обработки JSON-запросов
@@ -31,46 +33,55 @@ app.use(bodyParser.json());
 // для обработки кук
 app.use(cookieParser());
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {});
+mongoose.connect("mongodb://localhost:27017/mestodb", {});
+// подключаем логгер запросов
+app.use(requestLogger);
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
+// теперь обработка роутов
+app.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
   }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/(https?:\/\/)?(www\.)?[A-Za-z0-9-]*\.[A-Za-z0-9-]{2,}(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)*/),
+  login
+);
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(
+        /(https?:\/\/)?(www\.)?[A-Za-z0-9-]*\.[A-Za-z0-9-]{2,}(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)*/
+      ),
+    }),
   }),
-}), createUser);
-
+  createUser
+);
 app.use(auth);
-app.use('/', users);
-app.use('/', cards);
+app.use("/", users);
+app.use("/", cards);
 app.use((req, res, next) => {
-  next(new NotFoundError('Указанный путь не найден'));
+  next(new NotFoundError("Указанный путь не найден"));
 });
 
+// логгер ошибок
+app.use(errorLogger);
 // обработка ошибок celebrate
 app.use(errors());
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
 
-  res
-    .status(statusCode)
-    .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
+  res.status(statusCode).send({
+    // проверяем статус и выставляем сообщение в зависимости от него
+    message: statusCode === 500 ? "На сервере произошла ошибка" : message,
+  });
   next();
 });
 
